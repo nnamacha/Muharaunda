ecd0797726f48789cc8e9158c47a56b8f2753b46
nb181916 <NicholasN@Nedbank.co.za>
2021-10-02 06:56:06 +0200

    Mongo DB repository completed

--- Muharaunda.core/Contracts/IProfileRespository.cs
+++ Muharaunda.core/Contracts/IProfileRespository.cs
@@ -14,11 +14,11 @@ namespace Muharaunda.Core.Contracts
         public Task<ResponseModel<bool>> DeleteProfileAsync(int ProfileId);
         public Task<ResponseModel<ProfileBase>> GetProfileDetailsAsync(int ProfileId);
 
-        public Task<ResponseModel<ProfileBase>> AuthoriseProfileAsync(int ProfileId);
+        public ResponseModel<bool> AuthoriseProfile(int ProfileId);
         public Task<ResponseModel<ProfileBase>> GetListOfActiveProfilesAsync();
 
-        public Task<ResponseModel<Profile>> GetListOfDependentsByProfileAsync(int profileId);
-        public Task<ResponseModel<Profile>> GetNextOfKindByProfileAsync(int profileId);
+        public Task<ResponseModel<ProfileBase>> GetListOfDependentsByProfileAsync(int profileId);
+        public Task<ResponseModel<ProfileBase>> GetNextOfKindByProfileAsync(int profileId);
 
         public Task<ResponseModel<bool>> ValidateIdNumber(string IdNumber);
 
--- Munharaunda.Resources/Implementation/MongoDBRepository.cs
+++ Munharaunda.Resources/Implementation/MongoDBRepository.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static Muharaunda.Core.Constants.SystemWideConstants;
 using Profile = Muharaunda.Core.Models.Profile;
 
 namespace Munharaunda.Resources.Implementation
@@ -20,17 +21,42 @@ namespace Munharaunda.Resources.Implementation
 
         private readonly IMongoDatabase mongoDb;
         private readonly IMapper _mapper;
+        private readonly FilterDefinitionBuilder<Profile> filterBuilder;
+
+          
 
         public MongoDBRepository(IMongoClient client, IMapper mapper)
         {
 
             mongoDb = client.GetDatabase("KnowledgeBank");
+
             _mapper = mapper;
+
+            filterBuilder = Builders<Profile>.Filter;
         }
 
-        public Task<ResponseModel<ProfileBase>> AuthoriseProfileAsync(int ProfileId)
+        public ResponseModel<bool> AuthoriseProfile(int ProfileId)
         {
-            throw new NotImplementedException();
+            var response = CommonUtilites.GenerateResponseModel<bool>();
+            try
+            {
+                var updateBuilder = Builders<Profile>.Update;
+
+                updateBuilder.Set(u => u.ProfileStatus, ProfileStatuses.Active);
+
+                response.ResponseData.Add(true);
+
+
+            }
+            catch (Exception ex)
+            {
+
+                response.ResponseCode = ResponseConstants.R99;
+                response.ResponseMessage = ex.Message;
+                response.ResponseData.Add(false);
+            }
+
+            return response;
         }
 
         public async Task<ResponseModel<ProfileBase>> CreateProfileAsync(CreateProfileRequest request)
@@ -65,11 +91,36 @@ namespace Munharaunda.Resources.Implementation
 
             try
             {
-                var filter = Builders<Muharaunda.Core.Models.Profile>.Filter.Eq("ProfileId", profileId);
+                var filter = filterBuilder.Eq("ProfileId", profileId);
 
                 await mongoDb.GetCollection<Profile>("Profile").DeleteOneAsync(filter);
 
                 response.ResponseData.Add(true);
+            }
+            catch (Exception ex)
+            {
+
+                response.ResponseCode = ResponseConstants.R99;
+
+                response.ResponseMessage = ex.Message;
+            }
+
+            return response;
+        }
+
+
+
+        public async Task<ResponseModel<ProfileBase>> GetListOfActiveProfilesAsync()
+        {
+            var response = CommonUtilites.GenerateResponseModel<ProfileBase>();
+            try
+            {
+                var filter = filterBuilder.Eq("ProfileStatus", 0);
+
+                var profile = _mapper.Map<ProfileBase>(await mongoDb.GetCollection<Profile>("Profile").FindAsync(filter).Result.ToListAsync());
+
+
+                response.ResponseData.Add(profile);
             }
             catch (Exception ex)
             {
@@ -81,21 +132,52 @@ namespace Munharaunda.Resources.Implementation
             return response;
         }
 
-
-
-        public async Task<ResponseModel<ProfileBase>> GetListOfActiveProfilesAsync()
+        public async Task<ResponseModel<ProfileBase>> GetListOfDependentsByProfileAsync(int profileId)
         {
-            throw new NotImplementedException();
+            var response = CommonUtilites.GenerateResponseModel<ProfileBase>();
+            try
+            {
+                
+
+                var filter = filterBuilder.Where(u => u.ProfileType == ProfileTypes.Dependent && u.NextOfKin == profileId);
+
+                var profile = _mapper.Map<ProfileBase>(await mongoDb.GetCollection<Profile>("Profile").FindAsync(filter).Result.ToListAsync());
+
+
+                response.ResponseData.Add(profile);
+            }
+            catch (Exception ex)
+            {
+
+                response.ResponseCode = ResponseConstants.R99;
+                response.ResponseMessage = ex.Message;
+            }
+
+            return response;
         }
 
-        public Task<ResponseModel<Profile>> GetListOfDependentsByProfileAsync(int profileId)
+        public async Task<ResponseModel<ProfileBase>> GetNextOfKindByProfileAsync(int profileId)
         {
-            throw new NotImplementedException();
-        }
+            var response = CommonUtilites.GenerateResponseModel<ProfileBase>();
+            try
+            {
 
-        public Task<ResponseModel<Profile>> GetNextOfKindByProfileAsync(int profileId)
-        {
-            throw new NotImplementedException();
+
+                var filter = filterBuilder.Eq("NextOfKind", profileId);
+
+                var profile = _mapper.Map<ProfileBase>(await mongoDb.GetCollection<Profile>("Profile").FindAsync(filter).Result.ToListAsync());
+
+
+                response.ResponseData.Add(profile);
+            }
+            catch (Exception ex)
+            {
+
+                response.ResponseCode = ResponseConstants.R99;
+                response.ResponseMessage = ex.Message;
+            }
+
+            return response;
         }
 
         public async Task<ResponseModel<ProfileBase>> GetProfileDetailsAsync(int profileId)
@@ -145,7 +227,12 @@ namespace Munharaunda.Resources.Implementation
         public async Task<ResponseModel<bool>> ValidateIdNumber(string IdNumber)
         {
             var response = CommonUtilites.GenerateResponseModel<bool>();
-            response.ResponseData.Add(true);
+            var filter = Builders<Profile>.Filter.Eq("IdentificationNumber", IdNumber);
+
+            var profilesCount = await mongoDb.GetCollection<Profile>("Profile").Find(filter).CountDocumentsAsync();
+
+
+            response.ResponseData.Add(profilesCount == 0);
 
             return response;
         }
